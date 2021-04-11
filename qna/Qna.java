3e951e68ce564eff0280c443ec1e50370d795f6d
kim-1005 <81576710+kim-1005@users.noreply.github.com>
2021-04-11 17:31:35 +0900

    QNA
    QNA

---
+++ qna/Qna.java
@@ -0,0 +1,48 @@
+package qna;
+
+public class Qna {
+	
+	private int qnaID;
+	private String qnaTitle;
+	private String userID;
+	private String qnaDate;
+	private String qnaContent;
+	private int qnaAvailable;
+	
+	public int getQnaID() {
+		return qnaID;
+	}
+	public void setQnaID(int qnaID) {
+		this.qnaID = qnaID;
+	}
+	public String getQnaTitle() {
+		return qnaTitle;
+	}
+	public void setQnaTitle(String qnaTitle) {
+		this.qnaTitle = qnaTitle;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getQnaDate() {
+		return qnaDate;
+	}
+	public void setQnaDate(String qnaDate) {
+		this.qnaDate = qnaDate;
+	}
+	public String getQnaContent() {
+		return qnaContent;
+	}
+	public void setQnaContent(String qnaContent) {
+		this.qnaContent = qnaContent;
+	}
+	public int getQnaAvailable() {
+		return qnaAvailable;
+	}
+	public void setQnaAvailable(int qnaAvailable) {
+		this.qnaAvailable = qnaAvailable;
+	}
+}
